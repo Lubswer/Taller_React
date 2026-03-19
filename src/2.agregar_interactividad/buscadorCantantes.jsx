ef70e144ea6d604a8365bba31b18c2f7a6b629fa
lubswer <lubswer@hotmail.com>
2026-03-19 13:01:54 -0500

    Barra de busqueda funcional pero sin Optimizacion

--- src/2.agregar_interactividad/buscadorCantantes.jsx
+++ src/2.agregar_interactividad/buscadorCantantes.jsx
@@ -1,40 +1,23 @@
 import { cantantes } from "./cantantesData";
-import {useState} from "react";
+import { useState } from "react";
 
-export default function BuscadorCantantes (){
-    const [busqueda, setBusqueda] = useState("");
-    const [resultadoBusqueda, setResultadoBusqueda] = useState(false);
+export default function BuscadorCantantes() {
+  const [busqueda, setBusqueda] = useState("");
 
-    function buscarCantante(){
-
-        const resultadoBusqueda = cantantes.map(
-            (cantante) => {
-                if(cantante.nombre.toLowerCase().includes(busqueda.toLowerCase())){
-                    setResultadoBusqueda(!resultadoBusqueda);
-                    console.log(busqueda);
-                    return cantante;
-                }
-            }
-        )
-    return resultadoBusqueda;
-
-    }
-
-    return(
-        <div>
-        <span>Buscar</span>
-        <input type="text" value = {busqueda}
-         onChange={(e) => setBusqueda(e.target.value)}
-         />
-         {console.log(busqueda)}
-         <button onClick={() => {buscarCantante()}}>Buscar</button>
-         <button onClick={() => {setBusqueda("");
-            setResultadoBusqueda(false);
-         }}>Limpiar</button>
-         {resultadoBusqueda ? <p>El cantante existe</p> : <p> sin resultados </p>}
-        
-
-        </div>
-
-    )
+  return (
+    <div>
+      <span>Buscar</span>
+      <input
+        placeholder="Nombre del cantante"
+        type="text"
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+      />
+      <br />
+      {busqueda.trim().length === 0 && <p>Resultado de Busqueda</p>}
+      {busqueda.trim().length > 0 &&
+      cantantes.filter((cantante) => cantante.nombre.toLowerCase().includes(busqueda.toLowerCase())).map((cantante) => <p key={cantante.nombre}>{cantante.nombre}</p>)
+      }
+    </div>
+  );
 }
