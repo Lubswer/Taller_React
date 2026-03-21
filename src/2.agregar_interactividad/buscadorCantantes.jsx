5f396c512a589fafd499f03f724667bd644c8bd7
lubswer <lubswer@hotmail.com>
2026-03-20 19:00:26 -0500

    Complegidad inecesaria en el componente :(

--- src/2.agregar_interactividad/buscadorCantantes.jsx
+++ src/2.agregar_interactividad/buscadorCantantes.jsx
@@ -4,6 +4,24 @@ import { useState } from "react";
 export default function BuscadorCantantes() {
   const [busqueda, setBusqueda] = useState("");
 
+  function filtrarCantantes(paraBuscar) {
+      if(paraBuscar.trim().length === 0){
+        return"Resultados de Busqueda"
+      }
+      if(paraBuscar.trim().length > 0){
+        return cantantes.filter((cantante) => cantante.nombre.toLowerCase().includes(paraBuscar.toLowerCase())).map((cantante) => cantante.nombre)
+      }
+      return []
+  }
+  async function arrayCantantes(paraBuscar) {
+    let resultado = await filtrarCantantes(paraBuscar)
+
+    if(resultado.length === 0) {
+        resultado.push("No se encontraron resultados")
+    }
+    return resultado.forEach((cantante) => cantante);
+  }
+
   return (
     <div>
       <span>Buscar</span>
@@ -13,11 +31,12 @@ export default function BuscadorCantantes() {
         value={busqueda}
         onChange={(e) => setBusqueda(e.target.value)}
       />
-      <br />
-      {busqueda.trim().length === 0 && <p>Resultado de Busqueda</p>}
-      {busqueda.trim().length > 0 &&
-      cantantes.filter((cantante) => cantante.nombre.toLowerCase().includes(busqueda.toLowerCase())).map((cantante) => <p key={cantante.nombre}>{cantante.nombre}</p>)
-      }
+      <ul>
+        {arrayCantantes(busqueda).map((cantante, index) => (
+          <li key={index}>{cantante}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
